71b1070e57c6951bebb50f26dbb79d8fa1b0f0c1
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-09 19:42:03 +0100

    integrate the graphics scene and view and link it to the main window

---
+++ includes/scene.hpp
@@ -0,0 +1,17 @@
+#ifndef SCENE_H_
+#define SCENE_H_
+
+#include <QGraphicsScene>
+
+class Scene : public QGraphicsScene
+{
+
+public:
+
+    /**
+     * @brief scene constructor
+     */
+    Scene();
+};
+
+#endif
