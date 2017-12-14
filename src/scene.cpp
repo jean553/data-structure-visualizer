1402af3330a4dc3841b60a0c19d8088232ea3cb0
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-14 22:23:21 +0100

    improve the scene source and header

--- src/scene.cpp
+++ src/scene.cpp
@@ -9,6 +9,9 @@ class Scene::Impl
 public:
 
     LinkedList list;
+
+    qreal lastItemHorizontalPosition {10.0};
+    qreal lastItemVerticalPosition {10.0};
 };
 
 /**
@@ -26,24 +29,51 @@ Scene::~Scene() = default;
 /**
  *
  */
-void Scene::createLinkedList(const int& data)
+void Scene::createLinkedList(const int& data) &
 {
     LinkedListItem* item = new LinkedListItem(data);
+    applyCurrentItemPosition(item);
     addItem(item);
 
     impl->list = create(data);
+
+    increaseItemPositions();
 }
 
 /**
  *
  */
-void Scene::insertAtTheEndLinkedList(const int& data)
+void Scene::insertAtTheEndLinkedList(const int& data) &
 {
     LinkedListItem* item = new LinkedListItem(data);
+    applyCurrentItemPosition(item);
     addItem(item);
 
     insertAtTheEnd(
         &impl->list,
         data
     );
+
+    increaseItemPositions();
+}
+
+/**
+ *
+ */
+void Scene::applyCurrentItemPosition(QGraphicsTextItem* item) const &
+{
+    item->setPos(
+        impl->lastItemHorizontalPosition,
+        impl->lastItemVerticalPosition
+    );
+}
+
+/**
+ *
+ */
+void Scene::increaseItemPositions() const & noexcept
+{
+    constexpr qreal ITEMS_DISTANCE {50.0};
+    impl->lastItemHorizontalPosition += ITEMS_DISTANCE;
+    impl->lastItemVerticalPosition += ITEMS_DISTANCE;
 }
