d7345efa95f8a46f706cccdb4e9a0e26b4e14676
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-14 22:26:26 +0100

    improve the declarations of the methods of the main window

--- includes/mainwindow.hpp
+++ includes/mainwindow.hpp
@@ -29,12 +29,16 @@ private slots:
     /**
      * @brief called by the linked list menu create action;
      * displays the popup that asks for the linked list first value
+     *
+     * not const as the dialog creation modifies the main window
      */
     void createLinkedList();
 
     /**
      * @brief called by the linked list menu insert at the end action;
      * displays the popup that asks for the next linked list value
+     *
+     * not const as the dialog creation modifies the main window
      */
     void insertAtTheEndLinkedList();
 
