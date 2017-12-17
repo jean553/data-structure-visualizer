049c20141c70c16885ec0b801046e895787c9d7a
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-17 20:46:52 +0100

    display browsed linked list nodes when select an item

--- src/scene.cpp
+++ src/scene.cpp
@@ -83,10 +83,44 @@ const unsigned int Scene::getLinkedListLastIndex() const &
  */
 void Scene::selectItem(const int& index) &
 {
-    constexpr int LINKED_LIST_NODES_STEPS {2};
-
     auto allItems = items(Qt::AscendingOrder);
     const auto length = allItems.size();
+
+    constexpr int UNSELECTED_COLOR_RED {0};
+    constexpr int UNSELECTED_COLOR_GREEN {0};
+    constexpr int UNSELECTED_COLOR_BLUE {0};
+    constexpr int UNSELECTED_COLOR_ALPHA {255};
+    const auto unselectedColor = QColor(
+        UNSELECTED_COLOR_RED,
+        UNSELECTED_COLOR_GREEN,
+        UNSELECTED_COLOR_BLUE,
+        UNSELECTED_COLOR_ALPHA
+    );
+
+    constexpr int SELECTED_COLOR_RED {255};
+    constexpr int SELECTED_COLOR_GREEN {0};
+    constexpr int SELECTED_COLOR_BLUE {0};
+    constexpr int SELECTED_COLOR_ALPHA {255};
+    const auto selectedColor = QColor(
+        SELECTED_COLOR_RED,
+        SELECTED_COLOR_GREEN,
+        SELECTED_COLOR_BLUE,
+        SELECTED_COLOR_ALPHA
+    );
+
+    constexpr int BROWSED_COLOR_RED {0};
+    constexpr int BROWSED_COLOR_GREEN {0};
+    constexpr int BROWSED_COLOR_BLUE {255};
+    constexpr int BROWSED_COLOR_ALPHA {255};
+    const auto browsedColor = QColor(
+        BROWSED_COLOR_RED,
+        BROWSED_COLOR_GREEN,
+        BROWSED_COLOR_BLUE,
+        BROWSED_COLOR_ALPHA
+    );
+
+    constexpr int LINKED_LIST_NODES_STEPS {2};
+    const auto linkedListNodeIndex = index * LINKED_LIST_NODES_STEPS;
 
     /* we want to iterate to each linked list node to another,
        each one is separated by a step; handle this is easier
@@ -100,38 +134,19 @@ void Scene::selectItem(const int& index) &
            in case of a linked list, this situation should not happen */
         auto currentItem = static_cast<LinkedListItem*>(allItems.at(i));
 
-        constexpr int UNSELECTED_COLOR_RED {0};
-        constexpr int UNSELECTED_COLOR_GREEN {0};
-        constexpr int UNSELECTED_COLOR_BLUE {0};
-        constexpr int UNSELECTED_COLOR_ALPHA {255};
-        currentItem->setDefaultTextColor(
-            QColor(
-                UNSELECTED_COLOR_RED,
-                UNSELECTED_COLOR_GREEN,
-                UNSELECTED_COLOR_BLUE,
-                UNSELECTED_COLOR_ALPHA
-            )
-        );
+        QColor color = unselectedColor;
+
+        if (i < linkedListNodeIndex)
+        {
+            color = browsedColor;
+        }
+        else if (i == linkedListNodeIndex)
+        {
+            color = selectedColor;
+        }
+
+        currentItem->setDefaultTextColor(color);
     }
-
-    /* undefined behaviour if at(i) does not contain a pointer to a LinkedListItem*;
-       in case of a linked list, this situation should not happen */
-    auto selectedItem = static_cast<LinkedListItem*>(
-        allItems.at(index * LINKED_LIST_NODES_STEPS)
-    );
-
-    constexpr int SELECTED_COLOR_RED {255};
-    constexpr int SELECTED_COLOR_GREEN {0};
-    constexpr int SELECTED_COLOR_BLUE {0};
-    constexpr int SELECTED_COLOR_ALPHA {255};
-    selectedItem->setDefaultTextColor(
-        QColor(
-            SELECTED_COLOR_RED,
-            SELECTED_COLOR_GREEN,
-            SELECTED_COLOR_BLUE,
-            SELECTED_COLOR_ALPHA
-        )
-    );
 }
 
 /**
