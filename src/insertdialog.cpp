28a5a1e53f54ada1d62e64f87e5aabc8316ae9cf
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-21 21:23:58 +0100

    define insert after dialog content with two labels and two spin boxes for the index and the data

--- src/insertdialog.cpp
+++ src/insertdialog.cpp
@@ -1,13 +1,21 @@
 #include "insertdialog.hpp"
 
+#include <QLabel>
+#include <QSpinBox>
+#include <QGridLayout>
+
 class InsertDialog::Impl
 {
 
 public:
 
-    Impl()
-    {
-    }
+    QGridLayout* layout;
+
+    QLabel* indexLabel;
+    QLabel* dataLabel;
+
+    QSpinBox* indexLine;
+    QSpinBox* dataLine;
 };
 
 /**
@@ -15,6 +23,48 @@ public:
  */
 InsertDialog::InsertDialog() : impl(std::make_unique<Impl>())
 {
+    constexpr int WIDTH {200}; 
+    constexpr int HEIGHT {160};
+    setFixedSize(
+        WIDTH,
+        HEIGHT
+    );
+
+    setWindowTitle("Linked list");
+
+    auto& indexLabel = impl->indexLabel;
+    indexLabel = new QLabel("After index:", this);
+
+    auto& dataLabel = impl->dataLabel;
+    dataLabel = new QLabel("Data:", this);
+
+    /* FIXME: should depend of the list size */
+    constexpr int MAXIMUM_INDEX {100};
+    constexpr int MINIMUM_INDEX {0};
+    auto& indexLine = impl->indexLine;
+    indexLine = new QSpinBox(this);
+    indexLine->setRange(
+        MINIMUM_INDEX,
+        MAXIMUM_INDEX
+    );
+
+    constexpr int MAXIMUM_DATA {99};
+    constexpr int MINIMUM_DATA {0};
+    auto& dataLine = impl->dataLine;
+    dataLine = new QSpinBox(this);
+    dataLine->setRange(
+        MINIMUM_DATA,
+        MAXIMUM_DATA
+    );
+
+    auto& layout = impl->layout;
+    layout = new QGridLayout(this);
+    layout->addWidget(indexLabel, 0, 0);
+    layout->addWidget(indexLine, 1, 0);
+    layout->addWidget(dataLabel, 2, 0);
+    layout->addWidget(dataLine, 3, 0);
+
+    setLayout(layout);
 }
 
 /**
