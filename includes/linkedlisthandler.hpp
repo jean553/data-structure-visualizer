02d86844e5ea835538bba616633be76f6ab55eca
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-26 17:34:14 +0100

    remove the method that creates the double linked list from the linked list handler

--- includes/linkedlisthandler.hpp
+++ includes/linkedlisthandler.hpp
@@ -96,16 +96,6 @@ public:
     ) &;
 
     /**
-     * @brief called from the double linked list menu create action;
-     * create a new double linked list item
-     *
-     * @param data the data to insert into the first node
-     *
-     * TODO: should be moved into a double linked list scene
-     */
-    void createDoubleLinkedList(const int& data) const &;
-
-    /**
      * @brief clears the scene and updates it according to the linked list;
      * creates the required items and prints them on the scene
      *
