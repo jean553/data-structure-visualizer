784af7dfb8f89201d147d2309b49a22b3e5d9d31
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-29 08:41:27 +0100

    create a double linked list when call the double linked list creation action

--- includes/doublelinkedlisthandler.hpp
+++ includes/doublelinkedlisthandler.hpp
@@ -1,6 +1,8 @@
 #ifndef DOUBLELINKEDLISTHANDLER_H_
 #define DOUBLELINKEDLISTHANDLER_H_
 
+#include <memory>
+
 class DoubleLinkedListHandler
 {
 
@@ -12,12 +14,22 @@ public:
     DoubleLinkedListHandler();
 
     /**
+     * @brief default destructor
+     */
+    ~DoubleLinkedListHandler();
+
+    /**
      * @brief called from the double linked list menu create action;
      * create a new double linked list item
      *
      * @param data the data to insert into the first node
      */
     void createDoubleLinkedList(const int& data) const &;
+
+private:
+
+    class Impl;
+    std::unique_ptr<Impl> impl;
 };
 
 #endif
--- src/doublelinkedlisthandler.cpp
+++ src/doublelinkedlisthandler.cpp
@@ -1,15 +1,32 @@
 #include "doublelinkedlisthandler.hpp"
 
+#include "double_linked_list.h"
+
+class DoubleLinkedListHandler::Impl
+{
+
+public:
+
+    DoubleLinkedList list;
+};
+
 /**
  *
  */
-DoubleLinkedListHandler::DoubleLinkedListHandler()
+DoubleLinkedListHandler::DoubleLinkedListHandler() :
+    impl(std::make_unique<Impl>())
 {
 }
 
 /**
  *
  */
+DoubleLinkedListHandler::~DoubleLinkedListHandler() = default;
+
+/**
+ *
+ */
 void DoubleLinkedListHandler::createDoubleLinkedList(const int& data) const &
 {
+    impl->list = create(data);
 }
